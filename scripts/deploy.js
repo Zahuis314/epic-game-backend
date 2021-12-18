5e04a78730a2a3e898b785d921e18b9b9c9c1086
Roberto Zahuis <zahuis314@gmail.com>
2021-12-18 15:26:38 -0500

    Clean up deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,34 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+
     const gameContract = await gameContractFactory.deploy(
       ["Gaara", "Naruto", "Sasuke"],       // Names
       ["https://imgur.com/3wKJLD0.png", // Images
       "https://imgur.com/UYMUy3Z.png", 
       "https://imgur.com/izu6nf0.png"],
       [300, 200, 250],                    // HP values
-      [45, 100, 75]                       // Attack damage values
+      [45, 100, 75],                       // Attack damage values
+      "Madara", // Boss name
+      "https://imgur.com/fxzcijN.png", // Boss image
+      10000, // Boss hp
+      50 // Boss attack damage
     );
+
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   
-    
-    let txn;
-    txn = await gameContract.mintCharacterNFT(0);
-    await txn.wait();
-    console.log("Minted NFT #1");
+    // let txn;
+    // txn = await gameContract.mintCharacterNFT(2);
+    // await txn.wait();
+    // console.log("Minted NFT #1");
+
+    // txn = await gameContract.attackBoss();
+    // await txn.wait();
+
+    // txn = await gameContract.attackBoss();
+    // await txn.wait();
   
-    txn = await gameContract.mintCharacterNFT(1);
-    await txn.wait();
-    console.log("Minted NFT #2");
-  
-    txn = await gameContract.mintCharacterNFT(2);
-    await txn.wait();
-    console.log("Minted NFT #3");
-  
-    txn = await gameContract.mintCharacterNFT(1);
-    await txn.wait();
-    console.log("Minted NFT #4");
-  
-    console.log("Done deploying and minting!");
+    // console.log("Done deploying and minting!");
   };
   
   const runMain = async () => {
