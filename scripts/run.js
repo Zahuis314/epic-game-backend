bc9e88006fa9b575459e49e30c3d3fb3a2e71dac
Roberto Zahuis <zahuis314@gmail.com>
2021-12-18 15:12:37 -0500

    Add boss

--- scripts/run.js
+++ scripts/run.js
@@ -4,21 +4,38 @@ const main = async () => {
       ["Gaara", "Naruto", "Sasuke"],       // Names
       ["https://imgur.com/3wKJLD0.png", // Images
       "https://imgur.com/UYMUy3Z.png", 
-      "https://i.imgur.com/WMB6g9u.png"],
+      "https://imgur.com/izu6nf0.png"],
       [300, 200, 250],                    // HP values
-      [45, 100, 75]                       // Attack damage values
+      [45, 100, 75],                       // Attack damage values
+      "Madara", // Boss name
+      "https://imgur.com/fxzcijN.png", // Boss image
+      10000, // Boss hp
+      50 // Boss attack damage
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+
     let txn;
-    // We only have three characters.
-    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+  
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+    console.log("Minted NFT #2");
+  
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
+    console.log("Minted NFT #3");
 
-    // Get the value of the NFT's URI.
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("Token URI:", returnedTokenUri);
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
   };
   
   const runMain = async () => {
